c0a6ba968e67cb14e6368d3afdbd3e819c6e9d07
chaoshuai <huangchsh@ersoft.cn>
2021-03-10 19:43:13 +0800

    feat: axios 新增请求和响应拦截器

--- dist/utils.js
+++ dist/utils.js
@@ -23,14 +23,22 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "axios": () => (/* binding */ axios)
 /* harmony export */ });
 /* harmony import */ var _querystring__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./querystring */ "./src/querystring.js");
+/* harmony import */ var _is__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./is */ "./src/is.js");
+/* harmony import */ var _datahub__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./datahub */ "./src/datahub.js");
 
-const GET = 'GET',
-    POST = 'POST',
-    PUT = 'PUT',
-    DELETE = 'DELETE'
 
-function axios({
-    url,
+
+let db = (0,_datahub__WEBPACK_IMPORTED_MODULE_2__.createDataHub)()
+const GET  = 'GET',
+    POST   = 'POST',
+    PUT    = 'PUT',
+    DELETE = 'DELETE',
+    HEAD   = 'HEAD',
+    OPTION = 'OPTION',
+    PATCH  = 'PATCH'
+ 
+function request({
+    url ,
     method = GET,
     params = {},
     data   = {}
@@ -45,45 +53,121 @@ function axios({
         const xhr = new XMLHttpRequest()
         // xhrReq.open(method, url, async, user, password);
         xhr.open(method, url, true)
-        if(method === POST || method === PUT || method === DELETE){
-            xhr.setRequestHeader("Content-Type", "application/json")
-            xhr.send(JSON.stringify(data))
+        // xhr.setRequestHeader("Accept", "application/json, text/plain, */*")
+        if(method === POST || method === PUT || method === PATCH){
+            if((0,_is__WEBPACK_IMPORTED_MODULE_1__.isObject)(data)){
+                xhr.setRequestHeader("Content-Type", "application/json;charset=utf-8")
+                xhr.send(JSON.stringify(data))
+            }
         } else {
             xhr.send(null)
         }
         xhr.onreadystatechange = () => {
             if(xhr.readyState !== 4) return
-            if(xhr.status >= 200 && xhr.status <= 299){
-                let response
+            if(xhr.status >= 200 && xhr.status < 300){
+                let response;
                 try {
                     response = JSON.parse(xhr.response)
-                } catch (error) {
-                    response = xhr.response
-                }
+                } catch (e) {}
                 resolve({
                     status: xhr.status,
                     msg: xhr.statusText,
                     data: response
                 })
             } else {
-                reject(new Error(`oops... someting happened`))
+                reject(new Error(`oops... someting happened, Fails code ${xhr.status}`))
             }
         }
     })
 }
 
-axios.get = (url, options) => axios({
-    ...Object.assign({}, options, {url, method: GET})
+
+function guardRequest(config) {
+    let chains = [request, undefined]
+    let promese = Promise.resolve(config)
+
+    ;(db.get('request') || []).forEach( element => {
+        chains.unshift(element.resolved, element.rejected)
+    })
+    ;(db.get('response') || []).forEach( element => {
+        chains.push(element.resolved, element.rejected)
+    })
+
+    while(chains.length){
+        promese = promese.then(chains.shift(), chains.shift())
+    }
+    return promese
+}
+
+
+
+
+/* 请求和相应守卫 */
+axios.requestEach = (resolved, rejected) => {
+    db.set('request', {resolved, rejected})
+}
+axios.responseEach = (resolved, rejected) => {
+    db.set('response', {resolved, rejected})
+}
+
+
+
+/* 函数对象添加语法糖 */
+[GET, HEAD, DELETE, OPTION, /* 忽略data */ POST, PUT, PATCH ].forEach( method => {
+    axios[method.toLowerCase()] = (url, options) => axios({
+        ...Object.assign({}, options, {url, method})
+    })
 })
-axios.post = (url, options) => axios({
-    ...Object.assign({}, options, {url, method: POST})
-})
-axios.put = (url, options) => axios({
-    ...Object.assign({}, options, {url, method: PUT})
-})
-axios.delete = (url, options) => axios({
-    ...Object.assign({}, options, {url, method: DELETE})
-})
+
+function axios (config) {
+    return guardRequest(config)
+}
+
+/***/ }),
+
+/***/ "./src/datahub.js":
+/*!************************!*\
+  !*** ./src/datahub.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "createDataHub": () => (/* binding */ createDataHub)
+/* harmony export */ });
+function createDataHub () {
+    return {
+        hub: {},
+        set(symbol, data) {
+            if(!this.hub[symbol]){
+                this.hub[symbol] = []
+            }
+            this.hub[symbol].push(data)
+            return this.hub[symbol].length - 1
+        },
+        get(symbol, id) {
+            if(!this.hub[symbol]) return
+            if(id == null){
+                return this.hub[symbol]
+            } else {
+                return this.hub[symbol][id]
+            }
+            
+        },
+        eject(symbol, id) {
+            if(!this.hub[symbol]) return
+            if(id == null){
+                delete this.hub[symbol]
+            } else {
+                this.hub[symbol].splice(id, 1)
+            }
+        },
+        clean() {
+            this.hub = {}
+        }
+
+    }
+}
 
 /***/ }),
 
@@ -147,6 +231,73 @@ __webpack_require__.r(__webpack_exports__);
 function demo() {
     console.log('demo')
 }
+
+/***/ }),
+
+/***/ "./src/eventbus.js":
+/*!*************************!*\
+  !*** ./src/eventbus.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "createEventBus": () => (/* binding */ createEventBus)
+/* harmony export */ });
+function createEventBus() {
+    return {
+        hub: {},
+        emit (symbol, data) {
+            if(!this.hub[symbol]) return
+            this.hub[symbol].forEach(handler => handler(data));
+        },
+        on (symbol, handler) {
+            if(!this.hub[symbol]){
+                this.hub[symbol] = []
+            }
+            this.hub[symbol].push(handler)
+        },
+        off (symbol, cancelHandler) {
+            if(!this.hub[symbol]) return
+            if(!cancelHandler){
+                delete this.hub[symbol]
+            } else {
+                let index = this.hub[symbol].findIndex(handler => cancelHandler === handler)
+                if(index > -1) this.hub[symbol].splice(index, 1)
+            }
+        }
+
+    }
+}
+
+/***/ }),
+
+/***/ "./src/is.js":
+/*!*******************!*\
+  !*** ./src/is.js ***!
+  \*******************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "isPlainObject": () => (/* binding */ isPlainObject),
+/* harmony export */   "isObject": () => (/* binding */ isObject)
+/* harmony export */ });
+
+const toString = Object.prototype.toString
+
+function isPlainObject(val) {
+    if (toString.call(val) !== '[object Object]') {
+      return false;
+    }
+  
+    var prototype = Object.getPrototypeOf(val);
+    return prototype === null || prototype === Object.prototype;
+}
+
+function isObject(val) {
+    return val !== null && typeof val === 'object';
+  }
 
 /***/ }),
 
@@ -266,7 +417,11 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "axios": () => (/* reexport safe */ _axios__WEBPACK_IMPORTED_MODULE_2__.axios),
 /* harmony export */   "queryString": () => (/* reexport safe */ _querystring__WEBPACK_IMPORTED_MODULE_3__.queryString),
 /* harmony export */   "debounce": () => (/* reexport safe */ _debounce__WEBPACK_IMPORTED_MODULE_4__.debounce),
-/* harmony export */   "throttle": () => (/* reexport safe */ _throttle__WEBPACK_IMPORTED_MODULE_5__.throttle)
+/* harmony export */   "throttle": () => (/* reexport safe */ _throttle__WEBPACK_IMPORTED_MODULE_5__.throttle),
+/* harmony export */   "createEventBus": () => (/* reexport safe */ _eventbus__WEBPACK_IMPORTED_MODULE_6__.createEventBus),
+/* harmony export */   "createDataHub": () => (/* reexport safe */ _datahub__WEBPACK_IMPORTED_MODULE_7__.createDataHub),
+/* harmony export */   "isPlainObject": () => (/* reexport safe */ _is__WEBPACK_IMPORTED_MODULE_8__.isPlainObject),
+/* harmony export */   "isObject": () => (/* reexport safe */ _is__WEBPACK_IMPORTED_MODULE_8__.isObject)
 /* harmony export */ });
 /* harmony import */ var _demo__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./demo */ "./src/demo.js");
 /* harmony import */ var _deepClone__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./deepClone */ "./src/deepClone.js");
@@ -274,6 +429,12 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _querystring__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./querystring */ "./src/querystring.js");
 /* harmony import */ var _debounce__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./debounce */ "./src/debounce.js");
 /* harmony import */ var _throttle__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./throttle */ "./src/throttle.js");
+/* harmony import */ var _eventbus__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./eventbus */ "./src/eventbus.js");
+/* harmony import */ var _datahub__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ./datahub */ "./src/datahub.js");
+/* harmony import */ var _is__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ./is */ "./src/is.js");
+
+
+
 
 
 
--- src/axios.js
+++ src/axios.js
@@ -1,11 +1,17 @@
 import { queryString as qs} from './querystring'
-const GET = 'GET',
-    POST = 'POST',
-    PUT = 'PUT',
-    DELETE = 'DELETE'
-
-export function axios({
-    url,
+import { isObject } from './is'
+import { createDataHub } from './datahub'
+let db = createDataHub()
+const GET  = 'GET',
+    POST   = 'POST',
+    PUT    = 'PUT',
+    DELETE = 'DELETE',
+    HEAD   = 'HEAD',
+    OPTION = 'OPTION',
+    PATCH  = 'PATCH'
+ 
+function request({
+    url ,
     method = GET,
     params = {},
     data   = {}
@@ -20,42 +26,72 @@ export function axios({
         const xhr = new XMLHttpRequest()
         // xhrReq.open(method, url, async, user, password);
         xhr.open(method, url, true)
-        if(method === POST || method === PUT || method === DELETE){
-            xhr.setRequestHeader("Content-Type", "application/json")
-            xhr.send(JSON.stringify(data))
+        // xhr.setRequestHeader("Accept", "application/json, text/plain, */*")
+        if(method === POST || method === PUT || method === PATCH){
+            if(isObject(data)){
+                xhr.setRequestHeader("Content-Type", "application/json;charset=utf-8")
+                xhr.send(JSON.stringify(data))
+            }
         } else {
             xhr.send(null)
         }
         xhr.onreadystatechange = () => {
             if(xhr.readyState !== 4) return
-            if(xhr.status >= 200 && xhr.status <= 299){
-                let response
+            if(xhr.status >= 200 && xhr.status < 300){
+                let response;
                 try {
                     response = JSON.parse(xhr.response)
-                } catch (error) {
-                    response = xhr.response
-                }
+                } catch (e) {}
                 resolve({
                     status: xhr.status,
                     msg: xhr.statusText,
                     data: response
                 })
             } else {
-                reject(new Error(`oops... someting happened`))
+                reject(new Error(`oops... someting happened, Fails code ${xhr.status}`))
             }
         }
     })
 }
 
-axios.get = (url, options) => axios({
-    ...Object.assign({}, options, {url, method: GET})
+
+function guardRequest(config) {
+    let chains = [request, undefined]
+    let promese = Promise.resolve(config)
+
+    ;(db.get('request') || []).forEach( element => {
+        chains.unshift(element.resolved, element.rejected)
+    })
+    ;(db.get('response') || []).forEach( element => {
+        chains.push(element.resolved, element.rejected)
+    })
+
+    while(chains.length){
+        promese = promese.then(chains.shift(), chains.shift())
+    }
+    return promese
+}
+
+
+
+
+/* 请求和相应守卫 */
+axios.requestEach = (resolved, rejected) => {
+    db.set('request', {resolved, rejected})
+}
+axios.responseEach = (resolved, rejected) => {
+    db.set('response', {resolved, rejected})
+}
+
+
+
+/* 函数对象添加语法糖 */
+[GET, HEAD, DELETE, OPTION, /* 忽略data */ POST, PUT, PATCH ].forEach( method => {
+    axios[method.toLowerCase()] = (url, options) => axios({
+        ...Object.assign({}, options, {url, method})
+    })
 })
-axios.post = (url, options) => axios({
-    ...Object.assign({}, options, {url, method: POST})
-})
-axios.put = (url, options) => axios({
-    ...Object.assign({}, options, {url, method: PUT})
-})
-axios.delete = (url, options) => axios({
-    ...Object.assign({}, options, {url, method: DELETE})
-})
+
+export function axios (config) {
+    return guardRequest(config)
+}
